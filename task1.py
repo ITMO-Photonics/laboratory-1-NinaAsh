914c01451021734b72246ded80b8741e8d183bdc
NinaAsh <ninaashotovna@mail.ru>
2018-03-17 14:29:08 +0300

    message

--- task1.py
+++ task1.py
@@ -1,1 +1,8 @@
+import math 
+def calculate(R, L):
+    return math.pi*(R**2)*L
+    
+R = int(input('Please Enter the radius of a Cylinder: '))
+L = int(input('Please Enter the length of a Cylinder: '))
 
+print(calculate(R, L))
